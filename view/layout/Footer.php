281c944f644ee6e7dbbfee80f03c1fdb1a7322e0
ILIICI <viliici1988@gmail.com>
2021-05-28 11:23:16 +0100

    Uploade header + footer
    Updated bootstrap to 4.5 and redesign some bits and bobs

--- view/layout/Footer.php
+++ view/layout/Footer.php
@@ -1,5 +1,100 @@
-<footer>
-  <p>Author: vitali<br>
-  <a href="mailto:vitali@iliici.com">vitali@iliici.com</a></p>
-</footer>
-</html>
+<footer class="kilimanjaro_area">
+        <!-- Top Footer Area Start -->
+        <div class="foo_top_header_one section_padding_100_70">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="kilimanjaro_part">
+                            <h5>Despre noi</h5>
+                        <p>Officia est ea nostrud duis officia ullamco aliqua id ullamco qui excepteur aliqua. Reprehenderit nostrud pariatur occaecat non aliqua exercitation sit veniam commodo magna dolor dolore id. Id id fugiat excepteur amet laboris magna veniam voluptate adipisicing consectetur cillum cupidatat nostrud nulla. Reprehenderit duis occaecat deserunt laboris qui.</p>    
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="kilimanjaro_part">
+                            <h5>Rubrici</h5>
+                            <ul class=" kilimanjaro_widget">
+                                <li><a href="#">Classy</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Creative</a></li>
+                                <li><a href="#">One Page</a></li>
+                                <li><a href="#">Multipurpose</a></li>
+                                <li><a href="#">Minimal</a></li>
+                                <li><a href="#">Classic</a></li>
+                                <li><a href="#">Medical</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="kilimanjaro_part m-top-15">
+                            <h5>Link-uri utile</h5>
+                            <ul class="kilimanjaro_links">
+                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Terms & Conditions</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>About Licences</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Help & Support</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Careers</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Privacy Policy</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i>Community & Forum</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="kilimanjaro_part">
+                            <h5>Ultimile noutăți</h5>
+                            <div class="kilimanjaro_blog_area">
+                                <div class="kilimanjaro_thumb">
+								<img class="img-fluid" src="http://via.placeholder.com/100x50" alt="">
+
+                                </div>
+                                <a href="#">Title</a>
+                                <p class="kilimanjaro_date">21 Jan 2018</p>
+                                <p>Mollit est veniam amet nisi non.</p>
+                            </div>
+                            <div class="kilimanjaro_blog_area">
+                                <div class="kilimanjaro_thumb">
+								<img class="img-fluid" src="http://via.placeholder.com/100x50" alt="">
+                                </div>
+                                <a href="#">Title</a>
+                                <p class="kilimanjaro_date">21 Jan 2018</p>
+                                <p>Mollit est veniam amet nisi non.</p>
+                            </div>
+                            <div class="kilimanjaro_blog_area">
+                                <div class="kilimanjaro_thumb">
+								<img class="img-fluid" src="http://via.placeholder.com/100x50" alt="">
+                                </div>
+                                <a href="#">Title</a>
+                                <p class="kilimanjaro_date">21 Jan 2018</p>
+                                <p>Mollit est veniam amet nisi non.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="kilimanjaro_part">
+                            <h5>Contacte</h5>
+                            <div class="kilimanjaro_single_contact_info">
+                                <h5>Phone:</h5>
+                                <p>+440000000</p>
+                            </div>
+                            <div class="kilimanjaro_single_contact_info">
+                                <h5>Email:</h5>
+                                <p>vitali@iliici.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer Bottom Area Start -->
+        <div class="kilimanjaro_bottom_header_one section_padding_50 text-center">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <p>© All Rights Reserved by <a href="#">AMMB<i class="fa fa-love"></i></a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+<!-- Footer -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
